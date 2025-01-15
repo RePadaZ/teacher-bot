50c3fc6b41343ef7fa926b4d4901c407d66d9a7c
RePadaZ <padazbze007@gmail.com>
2025-01-15 07:08:11 +0300

    The main module has been rewritten to make it more convenient to work with

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/go-telegram/bot"
+	"log"
 	"os"
 	"os/signal"
 	"teacher-bot/pkg/system"
@@ -10,32 +11,51 @@ import (
 	"teacher-bot/src/handler"
 )
 
-func main() {
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer cancel()
-
-	// Получение токена
-	token := system.GetBotToken()
-
-	// Загрузка хэндлера по умолчанию для если не сработали другие
+// initializeBot создает и настраивает бота
+func initializeBot(token string) (*bot.Bot, error) {
 	opts := []bot.Option{
-		bot.WithDefaultHandler(handler.DefaultHandler),
-		bot.WithSkipGetMe(),
+		bot.WithDefaultHandler(handler.DefaultHandler), // Хэндлер по умолчанию
+		bot.WithSkipGetMe(),                            // Пропуск вызова getMe при запуске
 		bot.WithAllowedUpdates(bot.AllowedUpdates{
 			"message",
 		}),
-		bot.WithInitialOffset(int64(-2)),
+		bot.WithInitialOffset(int64(-2)), // Установка начального оффсета
 	}
 
-	// Старт бота
-	myBot, err := bot.New(token, opts...)
-	if err != nil {
-		panic(err.Error())
-	}
+	return bot.New(token, opts...)
+}
 
-	// Регистрируем наши хэндлеры
+// registerHandlers регистрирует хэндлеры для бота
+func registerHandlers(myBot *bot.Bot) {
 	myBot.RegisterHandlerMatchFunc(filter.IsStart, handler.Start)
 	myBot.RegisterHandlerMatchFunc(filter.IsHelp, handler.Help)
+}
 
+func main() {
+	// Настройка контекста с обработкой сигналов завершения
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	// Получение токена и проверка на его наличие
+	token := system.GetBotToken()
+	if token == "" {
+		log.Fatal("Bot token is missing. Please provide a valid token.")
+	}
+
+	// Инициализация бота
+	myBot, err := initializeBot(token)
+	if err != nil {
+		log.Fatalf("Failed to initialize bot: %v", err)
+	}
+
+	// Регистрация хэндлеров
+	registerHandlers(myBot)
+
+	log.Println("Bot is now running. Press CTRL-F2 to exit.")
+
+	// Запуск бота
 	myBot.Start(ctx)
+
+	// Логирование при завершении работы
+	log.Println("Bot stopped gracefully.")
 }
